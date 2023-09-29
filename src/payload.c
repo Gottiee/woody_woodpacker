89ac2088c013293d83abc0de855452e1bb530a4c
Gottiee <gottiedev@gmail.com>
2023-09-29 18:39:42 +0200

    need to do the patch of payload (entry)

--- src/payload.c
+++ src/payload.c
@@ -1,8 +1,20 @@
 #include "../include/woody.h"
 
+Elf64_Addr *get_strtab(Elf64_Ehdr *header, char *file_data)
+{
+
+}
+
+void find_text_section(t_data *data, t_exploit_data *exploit)
+{
+	Elf64_Shdr *seg = (void *)data->file_data + exploit->header->e_shoff;
+	Elf64_Addr strtab;
+
+	strtab = get_strtab(exploit->header, data->file_data);
+}
+
 void update_payload(t_exploit_data *exploit, t_data *data, t_payload *payload)
 {
-	(void)exploit;
-	(void)data;
+	find_text_section(data, exploit);
 	(void)payload;
 }
